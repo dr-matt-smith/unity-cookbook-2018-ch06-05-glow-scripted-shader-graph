70d4e0904b68c08797fe56aa6b4d6f27b54f8504
dr-matt-smith <dr_matt_smith@me.com>
2018-07-09 16:37:09 +0100

    added files to project

---
+++ Assets/_Scripts/GlowManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class GlowManager : MonoBehaviour
+{
+	private bool glow = false;
+	private string powerId = "Vector1_AA07C639";
+	void Update () 
+	{			
+		if (Input.GetKeyDown("1"))
+			glow = true;
+
+		if (Input.GetKeyDown("2"))
+			glow = false;
+
+		UpdateGlow(glow);
+	}
+
+	private void UpdateGlow(bool isGlowing)
+	{
+		// default - not glowing
+		float power = 0;
+
+		if (isGlowing)
+			power = 5;
+		
+		GetComponent<Renderer>().material.SetFloat(powerId, power);
+	}
+}
